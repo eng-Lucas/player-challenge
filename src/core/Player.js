22d0507336eca6c704783b706dc899d856101a91
lucas-d-silva-opl <engenharia_lucas@outlook.com>
2025-06-12 10:07:49 -0300

    implemented watchdog

--- src/core/Player.js
+++ src/core/Player.js
@@ -2,18 +2,20 @@ import fs from 'fs'
 import path from 'path'
 import Logger from '../lib/Logger.js'
 import VideoCache from '../lib/VideoCache.js'
+import Watchdog from '../lib/Watchdog.js'
 
 export default class Player {
   constructor() {
-    this.video1 = document.getElementById('video1')
-    this.video2 = document.getElementById('video2')
-    this.description = document.getElementById('description')
+    const video1 = document.getElementById('video1')
+    const video2 = document.getElementById('video2')
 
-    this.playlist = []
-    this.currentIndex = 0
-    this.currentVideo = this.video1
-    this.nextVideo = this.video2
-    this.transitionInProgress = false
+    this._description = document.getElementById('description')
+
+    this._playlist = []
+    this._currentIndex = 0
+    this._currentVideo = video1
+    this._nextVideo = video2
+    this._transitionInProgress = false
   }
 
   start() {
@@ -26,18 +28,18 @@ export default class Player {
     try {
       const data = fs.readFileSync(path.join(__dirname, '../../data/playlist.json'), 'utf-8')
       const json = JSON.parse(data)
-      this.playlist = json.playlist || []
+      this._playlist = json.playlist || []
 
-      Logger.log(`Playlist loaded with ${this.playlist.length} items.`)
+      Logger.log(`Playlist loaded with ${this._playlist.length} items.`)
     } catch (error) {
       Logger.error(`Error loading playlist: ${error}`)
     }
   }
 
   _swapVideos() {
-    const temp = this.currentVideo
-    this.currentVideo = this.nextVideo
-    this.nextVideo = temp
+    const temp = this._currentVideo
+    this._currentVideo = this._nextVideo
+    this._nextVideo = temp
   }
 
   _cleanupVideo(video) {
@@ -52,57 +54,70 @@ export default class Player {
   }
 
   async _playNext() {
-    if (this.playlist.length === 0) return
+    if (this._playlist.length === 0) {
+      return
+    }
 
-    const item = this.playlist[this.currentIndex]
-    this.description.textContent = item.description || ''
+    const item = this._playlist[this._currentIndex]
+    this._description.textContent = item.description || ''
 
-    this._cleanupVideo(this.nextVideo)
+    this._cleanupVideo(this._nextVideo)
 
     const localPath = await VideoCache.getVideoPath(item.url)
 
-    this.nextVideo.src = localPath
+    this._nextVideo.src = localPath
 
-    this.nextVideo.oncanplay = () => {
-      this.nextVideo.play().catch((err) => {
+    Watchdog.start(this._nextVideo, () => {
+        Logger.debug('Watchdog detected a freeze. Skipping to the next.')
+
+        this._skipToNext()
+      })
+
+    this._nextVideo.oncanplay = () => {
+      this._nextVideo.play().catch((err) => {
         Logger.error(`Error starting video: ${item.description} - ${item.url} ${err}`)
         this._skipToNext()
       })
     }
 
-    this.nextVideo.onplaying = () => {
-      if (this.transitionInProgress) return
-      this.transitionInProgress = true
+    this._nextVideo.onplaying = () => {
+      if (this._transitionInProgress) {
+        return
+      }
 
-      Logger.debug(`Playing: ${item.description} - ${this.nextVideo.src}`)
+      this._transitionInProgress = true
 
-      this.nextVideo.classList.add('visible')
-      this.currentVideo.classList.remove('visible')
+      Logger.debug(`Playing: ${item.description} - ${this._nextVideo.src}`)
+
+      this._nextVideo.classList.add('visible')
+      this._currentVideo.classList.remove('visible')
 
       // waits a while to allow the fade before cleaning up
       setTimeout(() => {
         Logger.debug(`Timeout, cleaning up currentVideo`)
 
-        this._cleanupVideo(this.currentVideo)
+        this._cleanupVideo(this._currentVideo)
         this._swapVideos()
 
         // Updates the new currentVideo (after swap) to handle the end
-        this.currentVideo.onended = () => {
-          Logger.debug(`Video finished: ${this.playlist[this.currentIndex].description}`)
-          this.transitionInProgress = false
+        this._currentVideo.onended = () => {
+          Logger.debug(`Video finished: ${this._playlist[this._currentIndex].description}`)
+          this._transitionInProgress = false
           this._skipToNext()
         }
       }, 600)
     }
 
-    this.nextVideo.onerror = () => {
+    this._nextVideo.onerror = () => {
       Logger.error(`Error loading video: ${item.url}`)
+
       this._skipToNext()
     }
   }
 
   _skipToNext() {
-    this.currentIndex = (this.currentIndex + 1) % this.playlist.length
+    this._transitionInProgress = false
+    this._currentIndex = (this._currentIndex + 1) % this._playlist.length
     this._playNext()
   }
 
@@ -111,8 +126,8 @@ export default class Player {
       if (document.visibilityState === 'visible') {
         Logger.debug('App returned to focus')
 
-        if (this.currentVideo && this.currentVideo.paused) {
-          this.currentVideo.play().catch(err => {
+        if (this._currentVideo && this._currentVideo.paused) {
+          this._currentVideo.play().catch(err => {
             Logger.error(`Error resuming video on return: ${err}`)
           })
         }
